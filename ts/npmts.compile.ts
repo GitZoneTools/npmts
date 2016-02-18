70213b04bc995282b7aa7c87ddf0369ab9db9c74
PhilKunz <github@philkunz.com>
2016-02-18 12:55:00 +0100

    disable declaration files for now

--- ts/npmts.compile.ts
+++ ts/npmts.compile.ts
@@ -76,7 +76,7 @@ module NpmtsCompile {
                         module: "commonjs"
                     }));
                 var stream = plugins.mergeStream([
-                    tsStream.dts.pipe(plugins.gulp.dest(outputDir)),
+                    //tsStream.dts.pipe(plugins.gulp.dest(outputDir)),
                     tsStream.js
                         .pipe(plugins.g.sourcemaps.write()) // Now the sourcemaps are added to the .js file
                         .pipe(plugins.g.header('#!/usr/bin/env node\n\n'))
