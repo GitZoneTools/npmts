0b6fc610e84e49beb923930ac6cef33e98e7d1fd
PhilKunz <github@philkunz.com>
2016-10-05 12:27:30 +0200

    made cli.js path discovery smarter

--- dist/npmts.assets.js
+++ dist/npmts.assets.js
@@ -3,12 +3,16 @@ const plugins = require("./npmts.plugins");
 const paths = require("./npmts.paths");
 const q = require("q");
 const npmts_promisechain_1 = require("./npmts.promisechain");
+const npmts_check_1 = require("./npmts.check");
 exports.run = function (configArg) {
     let done = q.defer();
     let config = configArg;
     npmts_promisechain_1.npmtsOra.text('now looking at ' + 'required assets'.yellow);
     if (config.cli === true) {
-        plugins.smartfile.fs.copySync(plugins.path.join(paths.npmtsAssetsDir, 'cli.js'), plugins.path.join(paths.distDir, 'cli.js'));
+        let mainJsPath = npmts_check_1.projectInfo.packageJson.main;
+        let cliJsString = plugins.smartfile.fs.toStringSync(plugins.path.join(paths.npmtsAssetsDir, 'cli.js'));
+        cliJsString = cliJsString.replace('{{pathToIndex}}', mainJsPath);
+        plugins.smartfile.memory.toFsSync(cliJsString, plugins.path.join(paths.distDir, 'cli.js'));
         plugins.beautylog.ok('installed CLI assets!');
         done.resolve(config);
     }
--- ts/npmts.assets.ts
+++ ts/npmts.assets.ts
@@ -2,17 +2,19 @@ import plugins = require('./npmts.plugins')
 import paths = require('./npmts.paths')
 
 import * as q from 'q'
-import {npmtsOra} from './npmts.promisechain'
+
+import { npmtsOra } from './npmts.promisechain'
+import { projectInfo } from './npmts.check'
 
 export var run = function(configArg){
     let done = q.defer()
     let config = configArg
     npmtsOra.text('now looking at ' + 'required assets'.yellow)
     if (config.cli === true) {
-        plugins.smartfile.fs.copySync(
-            plugins.path.join(paths.npmtsAssetsDir,'cli.js'),
-            plugins.path.join(paths.distDir,'cli.js')
-        )
+        let mainJsPath = projectInfo.packageJson.main
+        let cliJsString: string = plugins.smartfile.fs.toStringSync(plugins.path.join(paths.npmtsAssetsDir,'cli.js'))
+        cliJsString = cliJsString.replace('{{pathToIndex}}',mainJsPath)
+        plugins.smartfile.memory.toFsSync(cliJsString, plugins.path.join(paths.distDir,'cli.js'))
         plugins.beautylog.ok('installed CLI assets!')
         done.resolve(config)
     } else {
