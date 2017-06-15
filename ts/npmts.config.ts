29dff4c0e3c56367dccf4e867e4620be11d638c9
PhilKunz External <kunz@lossless-digital.com>
2017-06-15 23:42:48 +0000

    introduce a merge option

--- ts/npmts.config.ts
+++ ts/npmts.config.ts
@@ -3,28 +3,36 @@ import paths = require('./npmts.paths')
 
 import * as q from 'smartq'
 
-export type npmtsMode = 'default' | 'custom'
+/**
+ * specifies the different modes available
+ * default -> uses default options no matterm what
+ * merge -> uses merged default + custom options
+ * custom -> only uses specified options
+ */
+export type npmtsMode = 'default' | 'custom' | 'merge'
 
 export interface INpmtsConfig {
-  argv: any,
-  coverageTreshold: number,
-  mode: npmtsMode,
-  test: boolean,
-  testTs: any,
-  ts: any,
-  tsOptions: any,
+  argv: any
+  coverageTreshold: number
+  checkDependencies: boolean
+  mode: npmtsMode
+  test: boolean
+  testTs: any
+  ts: any
+  tsOptions: any
   watch: boolean
   runData: {
-    coverageLcovInfo?: string,
+    coverageLcovInfo?: string
     coverageResult?: number
   }
-};
+}
 
 export let run = function (argvArg) {
   let done = q.defer()
   let defaultConfig: INpmtsConfig = {
     argv: undefined,
     coverageTreshold: 70,
+    checkDependencies: true,
     mode: 'default',
     test: true,
     testTs: {},
@@ -50,16 +58,17 @@ export let run = function (argvArg) {
   switch (config.mode) {
     case 'default':
     case 'custom':
+    case 'merge':
       plugins.beautylog.ok('mode is ' + config.mode)
       done.resolve(config)
       break
     default:
-      plugins.beautylog.error(`mode not recognised!`)
+      plugins.beautylog.error(`mode not recognised! Can be default or custom`)
       process.exit(1)
   };
 
   // handle default mode
-  if (config.mode === 'default') {
+  if (config.mode === 'default' || config.mode === 'merge') {
     config.ts = {
       './ts/**/*.ts': './dist/'
     }
