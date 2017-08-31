d821a2efa6bce852704b4983e90ee8b68f701704
Phil Kunz <gitlab@philkunz.com>
2017-08-31 20:48:30 +0200

    update indention

--- ts/npmts.watch.ts
+++ ts/npmts.watch.ts
@@ -8,27 +8,27 @@ import { INpmtsConfig } from './npmts.config'
 
 let npmtsSmartchok: smartchok.Smartchok = null
 export let run = (configArg: INpmtsConfig) => {
-    let done = q.defer()
-    if (configArg.watch && npmtsSmartchok === null) {
-        let pathsToWatch: string[] = []
-        for (let key in configArg.ts) {
-            pathsToWatch.push(key)
-        }
-        for (let key in configArg.testTs) {
-            pathsToWatch.push(key)
-        }
-        npmtsSmartchok = new smartchok.Smartchok(pathsToWatch)
-        npmtsSmartchok.getObservableFor('change').then((changeObservableArg) => {
-            plugins.beautylog.info('now watching...')
-            changeObservableArg.subscribe(() => {
-                cli.run()
-            })
-        })
-        npmtsSmartchok.start()
-        done.resolve(configArg)
-    } else {
-        plugins.beautylog.info('not watching')
-        done.resolve(configArg)
+  let done = q.defer()
+  if (configArg.watch && npmtsSmartchok === null) {
+    let pathsToWatch: string[] = []
+    for (let key in configArg.ts) {
+      pathsToWatch.push(key)
     }
-    return done.promise
+    for (let key in configArg.testTs) {
+      pathsToWatch.push(key)
+    }
+    npmtsSmartchok = new smartchok.Smartchok(pathsToWatch)
+    npmtsSmartchok.getObservableFor('change').then((changeObservableArg) => {
+      plugins.beautylog.info('now watching...')
+      changeObservableArg.subscribe(() => {
+        cli.run()
+      })
+    })
+    npmtsSmartchok.start()
+    done.resolve(configArg)
+  } else {
+    plugins.beautylog.info('not watching')
+    done.resolve(configArg)
+  }
+  return done.promise
 }
